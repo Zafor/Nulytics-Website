4ad1801ed5b0141e99a6f4d250df6d3131dd7368
zaforiqbal <zafor@innovationsofts.com>
2019-12-29 17:01:17 +0600

    services mod

--- mail2.php
+++ mail2.php
@@ -19,7 +19,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "function.raaz@gmail.com";
+        $recipient = "zafor@nulytics.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
